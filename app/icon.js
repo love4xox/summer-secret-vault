6b19b56208fbcbd425865c1b2d4f29af7ce251c2
love4xox <tase3142@gmail.com>
2026-09-15 13:34:44 +0900

    feat: 하얀 하트 파비콘 적용

---
+++ app/icon.js
@@ -0,0 +1,32 @@
+import { ImageResponse } from "next/og";
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 22,
+          background: "#FCE8EC",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "50%",
+          border: "1.5px solid #F3BAC5",
+        }}
+      >
+        🤍
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
